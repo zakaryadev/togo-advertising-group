af9dd109badcd09c4bc15094216b7e72db6225ad
zakaryadev <muratbaevzakariya@gmail.com>
2026-08-18 17:33:54 +0500

    feat: add Google Tag Manager script for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono, Manrope, Onest } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 export const metadata: Metadata = {
   metadataBase: new URL("https://togogroup.uz"),
@@ -26,7 +27,24 @@ export default function RootLayout({
       lang="uz"
       className={`${onest.variable} ${manrope.variable} ${mono.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KRTLP8WX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KRTLP8WX');`}
+        </Script>
+      </body>
     </html>
   );
 }
